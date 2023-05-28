064d2e1342126731366eb7cece9ad410fc983154
TomasNagy <tomi.nagy@outlook.com>
2023-05-28 13:07:05 +0200

    Implement person structure

--- data_structure.c
+++ data_structure.c
@@ -4,18 +4,34 @@
 
 #define ARRAY_LENGTH 3
 
+typedef struct
+{
+    string name;
+    string phone;
+}
+person;
+
+
 int main(void)
 {
-    string name[ARRAY_LENGTH] = {"Tomas", "Eva", "Peter"};
-    string phone[ARRAY_LENGTH] = {"+1223", "+42987", "+891278"};
+    person people[ARRAY_LENGTH];
+
+    people[0].name = "Tomas"; 
+    people[0].phone = "+1223";
+
+    people[1].name = "Eva";
+    people[1].phone = "+42987";
+
+    people[2].name = "Peter"; 
+    people[2].phone =  "+891278";
 
     string get_user_name = get_string("Name: ");
 
     for (int i = 0; i < ARRAY_LENGTH; i++)
     {
-        if (strcmp(name[i], get_user_name) == 0)
+        if (strcmp(people[i].name, get_user_name) == 0)
         {
-            printf ("Found: %s\n", phone[i]);
+            printf ("Found: %s\n", people[i].phone);
             return 0;
         }
     }
