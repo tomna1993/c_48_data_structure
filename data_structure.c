4a960e730bf8b42d19272672ffa0a80babacfdbf
TomasNagy <tomi.nagy@outlook.com>
2023-05-28 12:57:50 +0200

    Implement finding phone number by name

--- data_structure.c
+++ data_structure.c
@@ -1,11 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+#define ARRAY_LENGTH 3
 
 int main(void)
 {
-    string name[] = {"Tomas", "Eva", "Peter"};
-    string phone[] = {"+1223", "+42987", "+891278"};
+    string name[ARRAY_LENGTH] = {"Tomas", "Eva", "Peter"};
+    string phone[ARRAY_LENGTH] = {"+1223", "+42987", "+891278"};
 
     string get_user_name = get_string("Name: ");
-    
+
+    for (int i = 0; i < ARRAY_LENGTH; i++)
+    {
+        if (strcmp(name[i], get_user_name) == 0)
+        {
+            printf ("Found: %s\n", phone[i]);
+            return 0;
+        }
+    }
+
+    printf ("Not found!\n");
+    return 1;
 }
